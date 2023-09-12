8afd138f0d15fcccc18243930506855b6abdc712
Renan Veronez Drechsler <renanvz@outlook.com>
2023-09-12 18:13:56 +0100

    Upload video

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Github } from "lucide-react"
+import { Github, FileVideo, Upload } from "lucide-react"
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
 
 export function App() {
   return (
@@ -37,7 +38,34 @@ export function App() {
               Lembre-se: Voce pode utilizar a variavel <code className="text-violet-400">{"{transcription}"}</code> no seu prompt para adicionar o conteudo da transcricao do video selecionado.
             </p>
           </div>
-          <aside className="w-80"></aside>
+          <aside className="w-80 space-y-6">
+            <form className="space-y-6">
+              <label
+                htmlFor="video"
+                className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+              >
+                <FileVideo className="w-4 h-4" />
+                Upload video
+              </label>
+              <input type="file" id="video" accept="video/mp4" className="sr-only" />
+
+              <Separator />
+
+              <div className="space-y-1">
+                <Label htmlFor="transcription_prompt">Transcript Prompt</Label>
+                <Textarea
+                  id="transcription_prompt"
+                  className="min-h-20 leading-relaxed"
+                  placeholder="Add kew words mentioned on video separated by commas (,)"
+                />
+              </div>
+
+              <Button type="submit" className="w-full">
+                Upload Video
+                <Upload className="w-4 h-4 ml-2" />
+              </Button>
+            </form>
+          </aside>
         </main>
       </div>
     </>
